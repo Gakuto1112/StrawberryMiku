938360449534cc07f12a704d81560df7c7cd3dbf
Gakuto1112 <gakuto131112@gmail.com>
2022-09-18 19:33:53 +0900

    バニラモデルの非表示

---
+++ scripts/main.lua
@@ -0,0 +1,9 @@
+events.ENTITY_INIT:register(function ()
+	--初期化処理
+	for _, vanillaModel in ipairs({vanilla_model.PLAYER, vanilla_model.ARMOR}) do
+		vanillaModel:setVisible(false)
+	end
+	for _, modelPart in ipairs({models.models.main.Head.HeadTails, models.models.main.Head.HeadTails.HeadTailRight, models.models.main.Head.HeadTails.HeadTailRight.HeadTailRight10, models.models.main.Head.HeadTails.HeadTailRight.HeadTailRight10.HeadTailRightRibbon, models.models.main.Head.HeadTails.HeadTailRight.HeadTailRight10.HeadTailRightRibbon.HeadTailRightRibbonLine, models.models.main.Head.HeadTails.HeadTailRight.HeadTailRightFlowers, models.models.main.Head.HeadTails.HeadTailLeft, models.models.main.Head.HeadTails.HeadTailLeft.HeadTailLeft10, models.models.main.Head.HeadTails.HeadTailLeft.HeadTailLeft10.HeadTailLeftRibbon, models.models.main.Head.HeadTails.HeadTailLeft.HeadTailLeft10.HeadTailLeftRibbon.HeadTailLeftRibbonLine, models.models.main.Head.HeadTails.HeadTailLeft.HeadTailLeftFlowers}) do
+		modelPart:setParentType("None")
+	end
+end)
