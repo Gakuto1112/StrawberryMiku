5cccd846378e1a59f01ea9bc9917dd0ed837da68
Gakuto1112 <gakuto131112@gmail.com>
2022-11-27 22:56:55 +0900

    プレイヤーの頭用のモデル作成

--- scripts/main.lua
+++ scripts/main.lua
@@ -4,14 +4,12 @@ events.ENTITY_INIT:register(function ()
 	DressClass = require("scripts/dress")
 	HairPhysicsClass = require("scripts/hair_physics")
 	ForkClass = require("scripts/forks")
-	NameplateClass = require("scripts/nameplate")
 
 	--初期化処理
 	for _, vanillaModel in ipairs({vanilla_model.PLAYER, vanilla_model.ARMOR}) do
 		vanillaModel:setVisible(false)
 	end
-	for _, modelPart in ipairs({models.models.main.Head.HeadTails, models.models.main.Head.HeadTails.HeadTailRightPivot, models.models.main.Head.HeadTails.HeadTailRightPivot.HeadTailRight, models.models.main.Head.HeadTails.HeadTailRightPivot.HeadTailRight.HeadTailRight10, models.models.main.Head.HeadTails.HeadTailRightPivot.HeadTailRight.HeadTailRight10.HeadTailRightRibbon, models.models.main.Head.HeadTails.HeadTailRightPivot.HeadTailRight.HeadTailRight10.HeadTailRightRibbon.HeadTailRightRibbonLine, models.models.main.Head.HeadTails.HeadTailRightPivot.HeadTailRight.HeadTailRightFlowers, models.models.main.Head.HeadTails.HeadTailLeftPivot, models.models.main.Head.HeadTails.HeadTailLeftPivot.HeadTailLeft, models.models.main.Head.HeadTails.HeadTailLeftPivot.HeadTailLeft.HeadTailLeft10, models.models.main.Head.HeadTails.HeadTailLeftPivot.HeadTailLeft.HeadTailLeft10.HeadTailLeftRibbon, models.models.main.Head.HeadTails.HeadTailLeftPivot.HeadTailLeft.HeadTailLeft10.HeadTailLeftRibbon.HeadTailLeftRibbonLine, models.models.main.Head.HeadTails.HeadTailLeftPivot.HeadTailLeft.HeadTailLeftFlowers}) do
-		modelPart:setParentType("None")
-	end
+	nameplate.ENTITY:setBackgroundColor(187 / 255, 40 / 255, 68 / 255)
+	nameplate.ENTITY.shadow = true
 	renderer:setShadowRadius(0.9)
 end)
--- scripts/nameplate.lua
+++
@@ -1,8 +0,0 @@
----@class NameplateClass ネームプレートを制御するクラス
-
-NameplateClass = {}
-
-nameplate.ENTITY:setBackgroundColor(187 / 255, 40 / 255, 68 / 255)
-nameplate.ENTITY.shadow = true
-
-return NameplateClass
